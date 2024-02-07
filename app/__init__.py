4c6bb25a51814107eb80571d199820b63aa4ad47
Rami Sillanpää <73886914+RamiSillanpaa@users.noreply.github.com>
2024-02-07 21:46:31 +0200

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,20 @@
+# app/__init__.py
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = os.urandom(24)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../varastonhallinta.db'
+
+    db.init_app(app)
+
+    from app.views import main
+    app.register_blueprint(main)
+
+    return app
 
