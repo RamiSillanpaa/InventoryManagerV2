ded4b4330c2f5950ede6d2bdd39ed252a51dce00
Rami Sillanpää <73886914+RamiSillanpaa@users.noreply.github.com>
2024-02-07 21:25:07 +0200

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -1,1 +1,11 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SelectField, IntegerField, SubmitField
+from wtforms.validators import DataRequired
 
+class ProductForm(FlaskForm):
+    mancode = StringField('Manufacturer Code', validators=[DataRequired()])
+    usercode = StringField('User Code', validators=[DataRequired()])
+    description = StringField('Description', validators=[DataRequired()])
+    category = SelectField('Category', choices=[('raw_material', 'Raw Material'), ('parts', 'Parts'), ('finished_product', 'Finished Product')], validators=[DataRequired()])
+    location = SelectField('Location', coerce=int, validators=[DataRequired()])
+    submit = SubmitField('Add Product')
