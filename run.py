dcf6b1912b05aa0c7244c67ffe2fc6a004de22f5
Rami Sillanpää <73886914+RamiSillanpaa@users.noreply.github.com>
2024-02-07 21:22:42 +0200

    Update run.py

--- run.py
+++ run.py
@@ -1,1 +1,7 @@
+from app import app, db
 
+if __name__ == '__main__':
+    db_path = 'varastonhallinta/varastonhallinta.db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + db_path
+    db.create_all()
+    app.run(debug=True)
